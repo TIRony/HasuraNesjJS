571f2f8992d4edbb63402fab1ddca7ddc9122c3b
TIRony <rony120114@gmail.com>
2023-02-14 12:13:25 +0600

    Authentication Done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const { NODEJS_PORT } = process.env;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-  console.log("Databse connected")
+  await app.listen(NODEJS_PORT);
+  console.log("Databse connected to " +NODEJS_PORT+ " port")
 }
 bootstrap();
